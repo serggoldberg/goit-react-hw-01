6074a5b25d7e27d68c0cd39e85d0c01faed71bff
Sergey Goldberg <117365819+serggoldberg@users.noreply.github.com>
2024-06-18 00:57:40 +0300

    ccs for third section

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,9 +2,9 @@ import css from './FriendListItem.module.css'
 
 export default function FriendListItem({ friend: {avatar, name, isOnline }}){
     return (
-        <div>
-            <img src= {avatar} alt="Avatar" width="48" />
-            <p>{name}</p>
+        <div className={css.card}>
+            <img className={css.avatar} src= {avatar} alt="Avatar" width="48" />
+            <p className={css.title}>{name}</p>
             <p className={isOnline === true ? css.online : css.offline}>{isOnline ? 'Online' : 'Offline'}</p>
         </div>
     );
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,24 @@
+import css from './TransactionHistory.module.css'
+
 export default function TransactionHistory ({items}){
     return(
-        <table>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+        <table className={css.table}>
+          <thead className={css.head}>
+            <tr className={css.text}>
+              <th className={css.columnHead}>Type</th>
+              <th className={css.columnHead}>Amount</th>
+              <th className={css.columnHead}>Currency</th>
+            </tr>
+          </thead>
 
-  <tbody>
-    {items.map(({type, amount, currency, id}) =>
-    <tr key={id}>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>)}
-  </tbody>
-</table>
+          <tbody>
+            {items.map(({type, amount, currency, id}) =>
+              <tr key={id}>
+                <td className={css.columnBody}>{type}</td>
+                <td className={css.columnBody}>{amount}</td>
+                <td className={css.columnBody}>{currency}</td>
+              </tr>)}
+          </tbody>
+        </table>
     ) 
 }
