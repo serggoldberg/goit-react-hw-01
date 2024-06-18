63f23c7e0cbba62a4f9f7cfb43355d48c70b8f53
Sergey Goldberg <117365819+serggoldberg@users.noreply.github.com>
2024-06-18 21:20:39 +0300

    css end

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -4,7 +4,7 @@ export default function TransactionHistory ({items}){
     return(
         <table className={css.table}>
           <thead className={css.head}>
-            <tr className={css.text}>
+            <tr>
               <th className={css.columnHead}>Type</th>
               <th className={css.columnHead}>Amount</th>
               <th className={css.columnHead}>Currency</th>
@@ -13,7 +13,7 @@ export default function TransactionHistory ({items}){
 
           <tbody>
             {items.map(({type, amount, currency, id}) =>
-              <tr key={id}>
+              <tr className={css.cells} key={id}>
                 <td className={css.columnBody}>{type}</td>
                 <td className={css.columnBody}>{amount}</td>
                 <td className={css.columnBody}>{currency}</td>
